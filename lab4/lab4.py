f87b72a7d5fadbbf5c463cbb68d6e9d4f6eeaf2a
Vinomo4 <victornovelle2000@gmail.com>
2020-10-29 17:21:27 +0100

    update

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -25,30 +25,34 @@ def dict_average(dict_list,k):
 #----------------------------------------------------------
 
 #Applies Rocchio's rule to the current query.
-'''
-def Rocchio(query_dict,response_dicts,alpha,beta,R,k):
+def Rocchio(query_dict,response_dicts,alpha,beta,k):
     dict_avg = dict_average(response_dicts,k)
-    aux_nw = 0
-    #   ESTO HAY QUE CAMBIARLO HEHE.
-    for term in dict_avg:
-        if term in query_dict:
-            query_dict[term] = alpha*query_dict[term] + beta*dict_avg[term]
-        elif aux_nw < R:
-            aux_nw += 1
-            query_dict[term] = beta*dict_avg[term]
-    return query_dict
-'''
-def Rocchio(query_dict,response_dicts,alpha,beta,R,k):
-    dict_avg = dict_average(response_dicts,k)
-    new_terms = []
+    query_dict = dict(TFIDF.normalize(query_dict.items()))
     for term in dict_avg:
         if term in query_dict:
             query_dict[term] = alpha*query_dict[term] + beta*dict_avg[term]
         else:
-            new_terms.append((term,dict_avg[term]))
-    new_terms.sort(key = lambda x: x[1], reverse = True)
-    new_terms = dict(new_terms[:k])
-    return query_dict, new_terms
+            query_dict[term] = beta*dict_avg[term]
+    return query_dict
+
+#Returns a dictionary containing top R elements from a dictionary
+def select_top_R(query_dict,R):
+    top_R = PriorityQueue(maxsize = R)
+    for term,weight in query_dict.items():
+        # Comparison with the least weighted term.
+        if top_R.full():
+            top = top_R.get()
+            if(weight > top[0]):
+                top_R.put((weight,term))
+            else: top_R.put(top)
+        else:
+            top_R.put((weight,term))
+    result = {}
+    while not top_R.empty():
+        aux = top_R.get()
+        result[aux[1]] = aux[0]
+    return result
+
 #Function that parses and selects the arguments.
 def parse_and_select():
     parser = argparse.ArgumentParser()
@@ -56,10 +60,9 @@ def parse_and_select():
     parser.add_argument('--nrounds', default=5, help='Number of iterations')
     parser.add_argument('--R', default=None, type = int, help='Maximum number of new terms')
     parser.add_argument('--k', default=5, type=int, help='Number of docs')
-    parser.add_argument('--alpha', default=0.5,type = float, help='Weight original query')
+    parser.add_argument('--alpha', default=1,type = float, help='Weight original query')
     parser.add_argument('--beta', default=0.5, type = float,help='Weight positive information')
     parser.add_argument('--query', default=None, nargs=argparse.REMAINDER, help='List of words to search')
-
     args = parser.parse_args()
 
     return args.index,args.nrounds,args.R,args.k,args.alpha,args.beta,args.query
@@ -93,47 +96,54 @@ if __name__ == '__main__':
         print('No query parameters passed!')
         raise SystemExit
     else:
-        print('QUERY INICIAL', query)
         query_dict = ini_query_dic(query)
 
     try:
-        # Iniciamos elasticsearch
+        # Start of elasticsearch
         client = Elasticsearch()
         s = Search(using=client, index=index)
 
-        # Resolvemos el query y en base a los k primeros resultados lo actualizamos
-        # mediante la regla de Rocchio nrounds veces.
+        # The query is solved and, using the k most relevant documents retrieved,
+        # the query is updated using the Rocchio's rule nrounds.
         for _ in range(nrounds):
+                #Creation of the query.
                 q = Q('query_string',query=query[0])
                 for i in range(1, len(query)):
-                    #CREO QUE FALLA AQUI AL PONER EL AND EN LA SEGUNDA ITERACION.
                     q &= Q('query_string',query=query[i])
 
                 s = s.query(q)
-                #Nos quedamos con los k documentos más relevantes
+                # We select the k most relevant documents.
                 response = s[0:k].execute()
-                print(len(response))
 
-                #Calculamos la representación en TFIDF para cada documento
-                #y las guardamos en una lista.
-                # No hace falta que los vectores esten ordenados en toTFIDF.
+                # We compute the TFIDF representation for each document and
+                #  store them in a list
                 response_dicts = []
                 for r in response:
-                    #Evitar repeticion codigo.
                     dr = dict(TFIDF.toTFIDF(client,index,TFIDF.search_file_by_path(client,index,r.path)))
                     response_dicts.append(dr)
 
-                query_dict, new_terms_dict = Rocchio(query_dict,response_dicts,alpha,beta,R,k)
+                # Application of the Roccio's rule to the previous query.
+                query_dict = Rocchio(query_dict,response_dicts,alpha,beta,k)
+
+                #Selection of the R terms with higher weight.
+                query_dict = select_top_R(query_dict,R)
+
+                print(query_dict)
+
+
                 query = []
                 for term,weight in query_dict.items():
                     query.append(term + '^' + str(weight))
 
-                for term,weight in new_terms_dict.items():
-                    query.append(term + '^' + str(weight))
+        # Number of documents found after the refination process.
 
-                print(query)
+        for r in response:  # only returns a specific number of results
+            print(f'ID= {r.meta.id} SCORE={r.meta.score}')
+            print(f'PATH= {r.path}')
+            print(f'TEXT: {r.text[:50]}')
+            print('-----------------------------------------------------------------')
 
-                print (f"{response.hits.total['value']} Documents")
+        print (f"{response.hits.total['value']} Documents")
 
     except NotFoundError:
         print(f'Index {index} does not exists')
